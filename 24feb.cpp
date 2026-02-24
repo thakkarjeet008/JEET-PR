62414b6b91fff576c3f84bf109a09f28f247db5f
thakkarjeet008 <thakkarjeet008@gmail.com>
2026-02-24 19:49:14 +0530

    add/sub

---
+++ 24feb.cpp
@@ -0,0 +1,15 @@
+// add and sub in c++
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 1. add two numbers
+   int a , b;
+    cout<<"\nEnter a = ";
+    cin>>a;
+    cout<<"\nEnter b = ";
+    cin>>b;
+    cout<<"\nsum of a and b = "<<a+b;
+
+} 
